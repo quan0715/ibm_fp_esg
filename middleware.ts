681c22f6eb80624c77b4c34fbd73a13d285d60dd
quan0715 <quan787887@gmail.com>
2024-07-15 17:09:44 +0800

    Bug: login redirect route

--- middleware.ts
+++ middleware.ts
@@ -1,44 +1,41 @@
 import { auth } from "@/lib/auth";
-import {NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 
-export const runtime = 'nodejs'
+export const runtime = "nodejs";
 
-const privateRoute: Array<string> = [
-    "/",
-    "/dashboard",
-]
+const privateRoute: Array<string> = ["/", "/dashboard"];
 
-const apiBaseRoute: string = "/api"
+const apiBaseRoute: string = "/api";
 
-const authBaseRoute = "/auth"
+const authBaseRoute = "/auth";
 
+export default auth(async (req) => {
+  const session = await auth();
+  // console.log('protected middleware')
+  // console.log(req.nextUrl.pathname)
+  // console.log('Session', session)
 
-export default auth( async (req)  =>{
-    const session = await auth()
-    // console.log('protected middleware')
-    // console.log(req.nextUrl.pathname)
-    // console.log('Session', session)
-
-    if(session?.user){
-        // console.log('User', session.user)
-        if(req.nextUrl.pathname.startsWith(authBaseRoute)){
-            return NextResponse.redirect(new URL('/dashboard/data', req.url))
-        }if(req.nextUrl.pathname.startsWith(apiBaseRoute)){
-            // console.log('API')
-            return null;
-        }
-        
-        return null;
+  if (session?.user) {
+    // console.log('User', session.user)
+    if (req.nextUrl.pathname.startsWith(authBaseRoute)) {
+      return NextResponse.redirect(
+        new URL("/dashboard/location_and_asset/location", req.url)
+      );
     }
-    else if(req.nextUrl.pathname.startsWith(authBaseRoute)){
-        return null;
-    } else {
-        return NextResponse.redirect(new URL('/auth/login', req.url))
+    if (req.nextUrl.pathname.startsWith(apiBaseRoute)) {
+      // console.log('API')
+      return null;
     }
-})
+
+    return null;
+  } else if (req.nextUrl.pathname.startsWith(authBaseRoute)) {
+    return null;
+  } else {
+    return NextResponse.redirect(new URL("/auth/login", req.url));
+  }
+});
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-    // matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-}
-
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  // matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
